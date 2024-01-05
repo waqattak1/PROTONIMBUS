ea9da61b4863c6ddc4f9c729da1945237c53cb27
Ali Khattak <waqas.khattak2016@gmail.com>
2024-01-05 11:58:00 -0500

    more navbar edits

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -5,7 +5,7 @@ import './Navbar.css'; // We will create this CSS file next
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <h1>Protonimbus</h1>
+      <h1>Protonimbus&reg;</h1>
       <div className="nav-links">
         <Link to="/">Home</Link>
       </div>
