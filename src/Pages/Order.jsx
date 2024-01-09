4748068457cf46b5e098da424b791ac6b31dc2a1
Ali Khattak <waqas.khattak2016@gmail.com>
2024-01-09 15:07:59 -0500

    order page

--- src/Pages/Order.jsx
+++ src/Pages/Order.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Order.css'; // CSS file for styling
-import orderVideo from '../Videos/orderVideo.mp4'; // Import your video
+
 
 const Order = () => {
   const amazonLink = "https://www.amazon.com/ProtoNiMBUS-Original-Multipurpose-Bottle-Caddy/dp/B0BNZJKHP1/ref=sr_1_1?crid=1JLRC4AGDY3RG&keywords=protonimbus%2Bgym%2Bcaddy&qid=1704830354&sprefix=protonimbus%2Bgym%2Bcaddy%2Caps%2C61&sr=8-1&th=1"; // Replace with your actual Amazon product link
