063b9f3a45b22875506c6e2f24e8c0716c6ea418
Ali Khattak <waqas.khattak2016@gmail.com>
2024-01-09 10:17:24 -0500

    Homepage edits

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -2,26 +2,25 @@ import React from 'react';
 import './About.css'; // Make sure to create a corresponding About.css file
 import Footer from '../Components/Footer'; // Assuming you use the same Footer component
 import video from '../Videos/animation.mp4'; // Import the video
+import photo from '../Images/MainPageImg.jpg';
 
 const About = () => {
   return (
     <div className="about-container">
-      {/* Video at the top */}
+      {/* Photo on the left */}
+      <div className="photo-container">
+        <img src={photo} alt="About Us" /> {/* Display the imported photo */}
+      </div>
+
+      {/* Video on the right */}
       <div className="video-container">
         <video className="about-video" autoPlay loop muted>
           <source src={video} type="video/mp4" />
           Your browser does not support the video tag.
         </video>
       </div>
-      
-      {/* Content of the About Page */}
-      <div className="about-content">
-        {/* Add your about page content here */}
-        <h1>About Us</h1>
-        <p>Welcome to our About Page. Here is some information about us...</p>
-        {/* More content */}
-      </div>
 
+      {/* Footer */}
       <Footer />
     </div>
   );
