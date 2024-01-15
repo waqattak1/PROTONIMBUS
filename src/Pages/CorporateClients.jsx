5a9fa78c74db6b5c73e4848c0937abed8f805574
Ali Khattak <waqas.khattak2016@gmail.com>
2024-01-15 14:41:49 -0500

    Corporate page

--- src/Pages/CorporateClients.jsx
+++ src/Pages/CorporateClients.jsx
@@ -1,17 +1,36 @@
 import React from 'react';
-import './CorporateClients.css'; // CSS file for styling
-import Footer from '../Components/Footer';
-
+import './CorporateClients.css'; // Ensure the CSS file is created
 
 const CorporateClients = () => {
- 
-
   return (
-    <div className="order-container">
-      <div className="order-button-container">    
-
+    <div className="corporate-clients-container">
+      <div className="message-container">
+        <p>We are delighted to offer our bulk order program for gym owners and corporate clients. Customize our caddy with your brand's colors and your logo.
+If you're a gym owner looking to enhance your facility, fill out the form below. Once you submit your information, our sales team will contact you promptly.</p>
       </div>
-      <Footer />
+      <div className="form-container">
+        <form>
+          <div className="form-field">
+            <label htmlFor="yourName">Your Name:</label>
+            <input type="text" id="yourName" name="yourName" />
+          </div>
+          <div className="form-field">
+            <label htmlFor="companyName">Company Name:</label>
+            <input type="text" id="companyName" name="companyName" />
+          </div>
+          <div className="form-field">
+            <label htmlFor="email">Email:</label>
+            <input type="email" id="email" name="email" />
+          </div>
+          <div className="form-field">
+            <label htmlFor="phone">Phone Number:</label>
+            <input type="tel" id="phone" name="phone" />
+          </div>
+          <div className="form-field">
+            <button type="submit" className="submit-button">Submit</button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
