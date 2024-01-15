64d48f629f4fbf4daaad4c71771e3f893dd3136d
Ali Khattak <waqas.khattak2016@gmail.com>
2024-01-15 10:16:12 -0500

    changes to home page

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -3,25 +3,21 @@ import './About.css';
 import Footer from '../Components/Footer';
 import video from '../Videos/animation.mp4';
 import photo from '../Images/MainPageImg.jpg';
-import newImage from '../Images/4.jpg'; // Import the new image
+import newImage from '../Images/4.jpg';
 
 const About = () => {
+  const amazonLink = "https://www.amazon.com/ProtoNiMBUS-Original-Multipurpose-Bottle-Caddy/dp/B0BNZJKHP1/ref=sr_1_1?crid=1JLRC4AGDY3RG&keywords=protonimbus%2Bgym%2Bcaddy&qid=1704830354&sprefix=protonimbus%2Bgym%2Bcaddy%2Caps%2C61&sr=8-1&th=1"; // Replace with your actual Amazon product link;
+
   return (
     <div className="about-container">
-
-      {/*Title*/}
       <div className="Title">
         <h1>Help keep the gym clean, without the hassle</h1>
       </div>
 
-      {/* Container for side-by-side photo and video */}
       <div className="media-container">
-        {/* Photo on the left */}
         <div className="photo-container">
           <img src={photo} alt="About Us" />
         </div>
-
-        {/* Video on the right */}
         <div className="video-container">
           <video className="about-video" autoPlay loop muted>
             <source src={video} type="video/mp4" />
@@ -30,12 +26,17 @@ const About = () => {
         </div>
       </div>
 
-      {/* New image container */}
-      <div className="new-image-container">
-        <img src={newImage} alt="New Image" />
+      <div className="image-and-order-container">
+        <div className="new-image-container">
+          <img src={newImage} alt="New Image" />
+        </div>
+        <div className="order-button-container">
+          <a href={amazonLink} target="_blank" rel="noopener noreferrer">
+            <button className="order-button">Order Now</button>
+          </a>
+        </div>
       </div>
 
-      {/* Footer */}
       <Footer />
     </div>
   );
